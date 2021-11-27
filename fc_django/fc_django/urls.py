663a86a1dceff71c5d805df83c1b3dcc163232de
Isak Song <thddltkr10@naver.com>
2021-11-27 17:24:36 +0900

    03.View-08.상품주문(1)

--- fc_django/fc_django/urls.py
+++ fc_django/fc_django/urls.py
@@ -1,22 +1,9 @@
-"""fc_django URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from fcuser.views import index, RegisterView, LoginView
 from product.views import ProductList, ProductCreate, ProductDetail
+from order.views import OrderCreate
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
@@ -25,4 +12,5 @@ urlpatterns = [
     path('product/', ProductList.as_view()),
     path('product/create/', ProductCreate.as_view()),
     path('product/<int:pk>/', ProductDetail.as_view()),
+    path('order/create/', OrderCreate.as_view()),
 ]
