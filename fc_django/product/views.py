c8d88184a050e27f34c2ba350b280fd1d1219e17
Isak Song <thddltkr10@naver.com>
2021-11-27 15:52:21 +0900

    03.view-05.상품등록하기(1)

--- fc_django/product/views.py
+++ fc_django/product/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Product
+from django.views.generic.edit import FormView
+from .forms import RegisterForm
 # Create your views here.
 
 
@@ -8,3 +10,9 @@ class ProductList(ListView):
     model = Product
     template_name = 'product.html'
     context_object_name = 'product_list'
+
+
+class ProductCreate(FormView):
+    form_class = RegisterForm
+    template_name = 'register_product.html'
+    success_url = '/product/'
